5ccb3d77b39ae62ba1997cf9e8ff168dd61ca478
barinek <barinek@initialcapacity.io>
2022-02-23 15:40:35 -0700

    Displaying azure policies
    Signed-off-by: barinek <barinek@initialcapacity.io>

--- pkg/providers/microsoftazure/azure_provider.go
+++ pkg/providers/microsoftazure/azure_provider.go
@@ -1,6 +1,7 @@
 package microsoftazure
 
 import (
+	"fmt"
 	"github.com/hexa-org/policy-orchestrator/pkg/orchestrator/provider"
 	"net/http"
 	"strings"
@@ -28,7 +29,23 @@ func (a AzureProvider) DiscoverApplications(info provider.IntegrationInfo) (apps
 }
 
 func (a AzureProvider) GetPolicyInfo(integrationInfo provider.IntegrationInfo, applicationInfo provider.ApplicationInfo) ([]provider.PolicyInfo, error) {
-	return []provider.PolicyInfo{}, nil
+	key := integrationInfo.Key
+	var policies []provider.PolicyInfo
+	if a.Http == nil {
+		a.Http = &http.Client{}
+	}
+	azureClient := AzureClient{a.Http}
+	principal, _ := azureClient.GetServicePrincipals(key, applicationInfo.Description)
+	assignments, _ := azureClient.GetAppRoleAssignedTo(key, principal.List[0].ID)
+	for _, a := range assignments.List {
+		policies = append(policies, provider.PolicyInfo{
+			Version: "0.2",
+			Action:  a.AppRoleId,
+			Subject: provider.SubjectInfo{AuthenticatedUsers: []string{fmt.Sprintf("%s:%s", a.PrincipalId, a.PrincipalDisplayName)}},
+			Object:  provider.ObjectInfo{Resources: []string{fmt.Sprintf("%s:%s", a.ResourceId, a.ResourceDisplayName)}},
+		})
+	}
+	return policies, nil
 }
 
 func (a AzureProvider) SetPolicyInfo(integrationInfo provider.IntegrationInfo, applicationInfo provider.ApplicationInfo, policyInfo provider.PolicyInfo) error {
