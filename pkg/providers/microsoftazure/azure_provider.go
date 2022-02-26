8979dc4b1250d4e70c39afb14e2d86fbf9ffec5b
barinek <barinek@initialcapacity.io>
2022-02-26 07:41:06 -0700

    Edit azure policies
    Signed-off-by: barinek <barinek@initialcapacity.io>

--- pkg/providers/microsoftazure/azure_provider.go
+++ pkg/providers/microsoftazure/azure_provider.go
@@ -18,9 +18,7 @@ func (g AzureProvider) Name() string {
 func (a AzureProvider) DiscoverApplications(info provider.IntegrationInfo) (apps []provider.ApplicationInfo, err error) {
 	key := info.Key
 	if strings.EqualFold(info.Name, a.Name()) {
-		if a.Http == nil {
-			a.Http = &http.Client{} // todo - for testing, might be a better way?
-		}
+		a.ensureClientIsAvailable()
 		azureClient := AzureClient{a.Http}
 		found, _ := azureClient.GetWebApplications(key)
 		apps = append(apps, found...)
@@ -31,23 +29,51 @@ func (a AzureProvider) DiscoverApplications(info provider.IntegrationInfo) (apps
 func (a AzureProvider) GetPolicyInfo(integrationInfo provider.IntegrationInfo, applicationInfo provider.ApplicationInfo) ([]provider.PolicyInfo, error) {
 	key := integrationInfo.Key
 	var policies []provider.PolicyInfo
-	if a.Http == nil {
-		a.Http = &http.Client{}
-	}
+	a.ensureClientIsAvailable()
 	azureClient := AzureClient{a.Http}
-	principal, _ := azureClient.GetServicePrincipals(key, applicationInfo.Description)
+	principal, _ := azureClient.GetServicePrincipals(key, applicationInfo.Description) // todo - description is named poorly
 	assignments, _ := azureClient.GetAppRoleAssignedTo(key, principal.List[0].ID)
-	for _, a := range assignments.List {
-		policies = append(policies, provider.PolicyInfo{
-			Version: "0.2",
-			Action:  a.AppRoleId,
-			Subject: provider.SubjectInfo{AuthenticatedUsers: []string{fmt.Sprintf("%s:%s", a.PrincipalId, a.PrincipalDisplayName)}},
-			Object:  provider.ObjectInfo{Resources: []string{fmt.Sprintf("%s:%s", a.ResourceId, a.ResourceDisplayName)}},
-		})
+
+	var appRoleId string
+	var principalIdsAndDisplayNames []string
+	var resourceIdAndDisplayName string
+
+	for _, assignment := range assignments.List {
+		appRoleId = assignment.AppRoleId
+		resourceIdAndDisplayName = fmt.Sprintf("%s:%s", assignment.ResourceId, assignment.ResourceDisplayName)
+		principalIdsAndDisplayNames = append(principalIdsAndDisplayNames, fmt.Sprintf("%s:%s", assignment.PrincipalId, assignment.PrincipalDisplayName))
 	}
+
+	policies = append(policies, provider.PolicyInfo{
+		Version: "0.2",
+		Action:  appRoleId,
+		Subject: provider.SubjectInfo{AuthenticatedUsers: principalIdsAndDisplayNames},
+		Object:  provider.ObjectInfo{Resources: []string{resourceIdAndDisplayName}},
+	})
+
 	return policies, nil
 }
 
 func (a AzureProvider) SetPolicyInfo(integrationInfo provider.IntegrationInfo, applicationInfo provider.ApplicationInfo, policyInfo provider.PolicyInfo) error {
-	return nil
+	key := integrationInfo.Key
+	a.ensureClientIsAvailable()
+	azureClient := AzureClient{a.Http}
+	principal, _ := azureClient.GetServicePrincipals(key, applicationInfo.Description) // todo - description is named poorly
+	var assignments []AzureAppRoleAssignment
+	resources := policyInfo.Object.Resources[0]
+	for _, user := range policyInfo.Subject.AuthenticatedUsers {
+		assignments = append(assignments, AzureAppRoleAssignment{
+			AppRoleId:   policyInfo.Action,
+			PrincipalId: strings.Split(user, ":")[0],
+			ResourceId:  strings.Split(resources, ":")[0],
+		})
+	}
+	return azureClient.SetAppRoleAssignedTo(key, principal.List[0].ID, assignments)
 }
+
+func (a AzureProvider) ensureClientIsAvailable() {
+	if a.Http == nil {
+		a.Http = &http.Client{} // todo - for testing, might be a better way?
+	}
+}
+
