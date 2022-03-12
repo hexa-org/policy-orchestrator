a2a8910f4387487ca519980f73c8b908f55c8009
barinek <barinek@initialcapacity.io>
2022-03-12 08:10:07 -0700

    Continued work on setting opa polocies
    Signed-off-by: barinek <barinek@initialcapacity.io>

--- pkg/providers/openpolicyagent/opa_provider_test.go
+++ pkg/providers/openpolicyagent/opa_provider_test.go
@@ -3,10 +3,15 @@ package openpolicyagent_test
 import (
 	"bytes"
 	"errors"
+	"fmt"
 	"github.com/hexa-org/policy-orchestrator/pkg/compressionsupport"
 	"github.com/hexa-org/policy-orchestrator/pkg/orchestrator/provider"
 	"github.com/hexa-org/policy-orchestrator/pkg/providers/openpolicyagent"
+	"github.com/hexa-org/policy-orchestrator/pkg/providers/openpolicyagent/test"
 	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+	"math/rand"
+	"os"
 	"path/filepath"
 	"runtime"
 	"testing"
@@ -19,7 +24,7 @@ func TestDiscoverApplications(t *testing.T) {
 }
 `)
 	p := openpolicyagent.OpaProvider{}
-	applications, _ := p.DiscoverApplications(provider.IntegrationInfo{Name: "open_policy_agent", Key:  key,})
+	applications, _ := p.DiscoverApplications(provider.IntegrationInfo{Name: "open_policy_agent", Key: key})
 	assert.Equal(t, 1, len(applications))
 	assert.Equal(t, "package authz", applications[0].Name)
 	assert.Equal(t, "Open policy agent bundle", applications[0].Description)
@@ -37,7 +42,7 @@ func TestGetPolicyInfo(t *testing.T) {
 	var buffer bytes.Buffer
 	_ = compressionsupport.Gzip(&buffer, tar)
 
-	mockClient := MockClient{response: buffer.Bytes()}
+	mockClient := openpolicyagent_test.MockClient{Response: buffer.Bytes()}
 	client := openpolicyagent.BundleClient{HttpClient: &mockClient}
 
 	resourcesDirectory := filepath.Join(file, "../resources")
@@ -62,8 +67,8 @@ func TestGetPolicyInfo_withBadRequest(t *testing.T) {
   "bundle_url": "aBigUrl"
 }
 `)
-	mockClient := MockClient{}
-	mockClient.err = errors.New("oops")
+	mockClient := openpolicyagent_test.MockClient{}
+	mockClient.Err = errors.New("oops")
 	_, file, _, _ := runtime.Caller(0)
 	client := openpolicyagent.BundleClient{HttpClient: &mockClient}
 	service := openpolicyagent.OpaService{ResourcesDirectory: filepath.Join(file, "../resources")}
@@ -78,8 +83,8 @@ func TestGetPolicyInfo_withBadResourceDir(t *testing.T) {
   "bundle_url": "aBigUrl"
 }
 `)
-	mockClient := MockClient{}
-	mockClient.err = errors.New("oops")
+	mockClient := openpolicyagent_test.MockClient{}
+	mockClient.Err = errors.New("oops")
 	client := openpolicyagent.BundleClient{HttpClient: &mockClient}
 	service := openpolicyagent.OpaService{}
 	p := openpolicyagent.OpaProvider{Client: client, Service: service}
@@ -88,9 +93,53 @@ func TestGetPolicyInfo_withBadResourceDir(t *testing.T) {
 }
 
 func TestSetPolicyInfo(t *testing.T) {
+	key := []byte(`
+{
+  "bundle_url": "aBigUrl"
+}
+`)
+	mockClient := openpolicyagent_test.MockClient{}
+	client := openpolicyagent.BundleClient{HttpClient: &mockClient}
+
+	_, file, _, _ := runtime.Caller(0)
+	resourcesDirectory := filepath.Join(file, "../resources")
+	service := openpolicyagent.OpaService{ResourcesDirectory: resourcesDirectory}
+
+	p := openpolicyagent.OpaProvider{Client: client, Service: service}
+	err := p.SetPolicyInfo(
+		provider.IntegrationInfo{Name: "open_policy_agent", Key: key},
+		provider.ApplicationInfo{},
+		provider.PolicyInfo{Version: "0.1", Action: "GET", Subject: provider.SubjectInfo{AuthenticatedUsers: []string{"allusers"}}, Object: provider.ObjectInfo{Resources: []string{"/"}}},
+	)
+	assert.NoError(t, err)
+}
+
+func TestMakeDefaultBundle(t *testing.T) {
 	client := openpolicyagent.BundleClient{}
 	service := openpolicyagent.OpaService{}
 	p := openpolicyagent.OpaProvider{Client: client, Service: service}
-	err := p.SetPolicyInfo(provider.IntegrationInfo{}, provider.ApplicationInfo{}, provider.PolicyInfo{})
-	assert.NoError(t, err)
+
+	rego := []byte(`package authz
+import future.keywords.in
+default allow = false
+allow {
+	input.method = "GET"
+	input.path in ["/"]
+	input.principals[_] in ["allusers"]
+}`)
+	bundle, _ := p.MakeDefaultBundle(rego)
+
+	gzip, _ := compressionsupport.UnGzip(bytes.NewReader(bundle.Bytes()))
+	path := filepath.Join(os.TempDir(), fmt.Sprintf("/test-bundle-%d", rand.Uint64()))
+	_ = compressionsupport.UnTarToPath(bytes.NewReader(gzip), path)
+
+	created, _ := ioutil.ReadFile(filepath.Join(path, "/bundle/policy.rego"))
+	assert.Contains(t, string(created), "input.principals[_] in [\"allusers\"]")
+	assert.NotContains(t, string(created), "input.principals[_] in [\"humanresources@\"]")
+
+	mcreated, _ := ioutil.ReadFile(filepath.Join(path, "/bundle/.manifest"))
+	assert.Contains(t, string(mcreated), "{\"revision\":\"\",\"roots\":[\"\"]}")
+
+	dcreated, _ := ioutil.ReadFile(filepath.Join(path, "/bundle/data.json"))
+	assert.Contains(t, string(dcreated), "{}")
 }
---
+++ pkg/providers/openpolicyagent/test/double.go
@@ -0,0 +1,28 @@
+package openpolicyagent_test
+
+import (
+	"bytes"
+	"github.com/stretchr/testify/mock"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+)
+
+type MockClient struct {
+	mock.Mock
+	Request     []byte
+	Response    []byte
+	Err         error
+}
+
+func (m *MockClient) Get(_ string) (resp *http.Response, err error) {
+	recorder := httptest.ResponseRecorder{}
+	recorder.Body = bytes.NewBuffer(m.Response)
+	return recorder.Result(), m.Err
+}
+
+func (m *MockClient) Post(_, contentType string, body io.Reader) (resp *http.Response, err error) {
+	m.Request, _ = io.ReadAll(body)
+	return &http.Response{StatusCode: 200, Body: ioutil.NopCloser(bytes.NewReader(m.Response))}, m.Err
+}
