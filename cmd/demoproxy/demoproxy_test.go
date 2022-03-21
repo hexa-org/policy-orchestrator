01d527e87d56b0ff1a371be4e6501d4511d8c016
barinek <barinek@initialcapacity.io>
2022-03-20 20:31:24 -0600

    Demo app proxy with async worker
    Signed-off-by: barinek <barinek@initialcapacity.io>

--- cmd/demoproxy/demoproxy_test.go
+++ cmd/demoproxy/demoproxy_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"github.com/gorilla/mux"
 	"github.com/hexa-org/policy-orchestrator/pkg/healthsupport"
@@ -15,30 +16,26 @@ import (
 	"testing"
 )
 
-func TestApp(t *testing.T) {
+type ErroneousWorker struct {
+	Err error
+}
+
+func (m *ErroneousWorker) Run(i interface{}) error {
+	m.Err = errors.New("oops")
+	return m.Err
+}
+
+func TestProxy(t *testing.T) {
 
 	listener, _ := net.Listen("tcp", "localhost:0")
-	app := websupport.Create(listener.Addr().String(), func(x *mux.Router) {
-		x.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-			_, _ = w.Write([]byte("success"))
-		})
-		x.HandleFunc("/plus", func(w http.ResponseWriter, r *http.Request) {
-			_, _ = w.Write([]byte("plus"))
-		})
-	}, websupport.Options{})
-	go func() {
-		websupport.Start(app, listener)
-	}()
-	healthsupport.WaitForHealthy(app)
-
-	///
+	app := aSmallTestApp(listener)
 
 	remoteUrl := fmt.Sprintf("http://%s", listener.Addr().String())
 
 	_, file, _, _ := runtime.Caller(0)
 	resourcesDirectory := filepath.Join(file, "../resources")
 	proxyListener, _ := net.Listen("tcp", "localhost:0")
-	proxyApp := App(remoteUrl, proxyListener.Addr().String(), resourcesDirectory)
+	proxyApp := App(remoteUrl, proxyListener.Addr().String(), resourcesDirectory, IdentityWorker{})
 	go func() {
 		websupport.Start(proxyApp, proxyListener)
 	}()
@@ -60,7 +57,54 @@ func TestApp(t *testing.T) {
 	websupport.Stop(app)
 }
 
+func TestProxy_WithError(t *testing.T) {
+
+	worker := &ErroneousWorker{}
+
+	listener, _ := net.Listen("tcp", "localhost:0")
+	app := aSmallTestApp(listener)
+
+	remoteUrl := fmt.Sprintf("http://%s", listener.Addr().String())
+
+	_, file, _, _ := runtime.Caller(0)
+	resourcesDirectory := filepath.Join(file, "../resources")
+	proxyListener, _ := net.Listen("tcp", "localhost:0")
+	proxyApp := App(remoteUrl, proxyListener.Addr().String(), resourcesDirectory, worker)
+	go func() {
+		websupport.Start(proxyApp, proxyListener)
+	}()
+	healthsupport.WaitForHealthy(proxyApp)
+
+	_, _ = http.Get(fmt.Sprintf("http://%s/", proxyApp.Addr))
+	assert.Equal(t, "oops", worker.Err.Error())
+
+	websupport.Stop(proxyApp)
+	websupport.Stop(app)
+}
+
+func TestIdentityWorker_Run(t *testing.T) {
+	err := IdentityWorker{}.Run([]IdentityInfo{{"someWork"}, {"someOtherWork"}})
+	assert.NoError(t, err)
+}
+
 func TestConfigWithPort(t *testing.T) {
 	_ = os.Setenv("PORT", "0")
+	_ = os.Setenv("REMOTER_URL", "http://localhost:8886")
 	newApp("localhost:0")
+}
+
+func aSmallTestApp(listener net.Listener) *http.Server {
+	app := websupport.Create(listener.Addr().String(), func(x *mux.Router) {
+		x.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			_, _ = w.Write([]byte("success"))
+		})
+		x.HandleFunc("/plus", func(w http.ResponseWriter, r *http.Request) {
+			_, _ = w.Write([]byte("plus"))
+		})
+	}, websupport.Options{})
+	go func() {
+		websupport.Start(app, listener)
+	}()
+	healthsupport.WaitForHealthy(app)
+	return app
 }
