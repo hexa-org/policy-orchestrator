30b6277e63882766eb2661d0a5004ca8e591ba34
barinek <barinek@initialcapacity.io>
2022-03-20 20:31:24 -0600

    Demo app proxy with async worker
    Signed-off-by: barinek <barinek@initialcapacity.io>

--- cmd/demoproxy/demoproxy.go
+++ cmd/demoproxy/demoproxy.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/gorilla/mux"
 	"github.com/hexa-org/policy-orchestrator/pkg/websupport"
+	"github.com/hexa-org/policy-orchestrator/pkg/workflowsupport"
 	"log"
 	"net"
 	"net/http"
@@ -18,30 +19,50 @@ import (
 type ProxySupport struct {
 	remote  *url.URL
 	reverse *httputil.ReverseProxy
+	worker  workflowsupport.Worker
 }
 
-func NewProxySupport(remote *url.URL) *ProxySupport {
+func NewProxySupport(remote *url.URL, worker workflowsupport.Worker) *ProxySupport {
 	log.Println("Enabling proxy middleware.")
-	return &ProxySupport{remote, httputil.NewSingleHostReverseProxy(remote)}
+	return &ProxySupport{remote, httputil.NewSingleHostReverseProxy(remote), worker}
 }
 
 func (p *ProxySupport) Middleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if path := r.URL.Path; (strings.Index(path, "/health") != 0 && strings.Index(path, "/_proxy") != 0) {
+		if path := r.URL.Path; strings.Index(path, "/health") != 0 && strings.Index(path, "/_proxy") != 0 {
 			log.Println("Proxying request.")
 			r.URL.Host = p.remote.Host
 			r.URL.Scheme = p.remote.Scheme
 			r.Header.Set("X-Forwarded-Host", r.Header.Get("Host"))
 			r.Host = p.remote.Host
+			go func() {
+				err := p.worker.Run([]IdentityInfo{{"someWork"}})
+				if err != nil {
+					log.Println("Trouble, unable to run async work.")
+				}
+			}()
 			p.reverse.ServeHTTP(w, r)
+		} else {
+			next.ServeHTTP(w, r)
 		}
-		next.ServeHTTP(w, r)
 	})
 }
 
-///
+type IdentityInfo struct {
+	info string
+}
 
-func App(remoteUrl string, addr string, resourcesDirectory string) *http.Server {
+type IdentityWorker struct {
+}
+
+func (i IdentityWorker) Run(work interface{}) error {
+	for _, info := range work.([]IdentityInfo) {
+		log.Printf("Attempting demo app proxy async work. %s\n", info.info)
+	}
+	return nil
+}
+
+func App(remoteUrl string, addr string, resourcesDirectory string, worker workflowsupport.Worker) *http.Server {
 	foundUrl, err := url.Parse(remoteUrl)
 	if err != nil {
 		panic(err)
@@ -49,7 +70,7 @@ func App(remoteUrl string, addr string, resourcesDirectory string) *http.Server 
 	basic := NewBasicApp(foundUrl)
 	server := websupport.Create(addr, basic.loadHandlers(), websupport.Options{ResourceDirectory: resourcesDirectory})
 	router := server.Handler.(*mux.Router)
-	proxySupport := NewProxySupport(foundUrl)
+	proxySupport := NewProxySupport(foundUrl, worker)
 	router.Use(proxySupport.Middleware)
 	return server
 }
@@ -91,7 +112,7 @@ func newApp(addr string) (*http.Server, net.Listener) {
 	_, file, _, _ := runtime.Caller(0)
 	resourcesDirectory := filepath.Join(file, "../resources")
 	listener, _ := net.Listen("tcp", addr)
-	return App(remoteUrl, listener.Addr().String(), resourcesDirectory), listener
+	return App(remoteUrl, listener.Addr().String(), resourcesDirectory, IdentityWorker{}), listener
 }
 
 func main() {
