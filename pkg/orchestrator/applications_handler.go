15737b250a45e03234de682055f622a51a56faf6
Phil Hunt <phil.hunt@independentid.com>
2022-04-11 18:07:56 -0700

    Connection diagnostic improvements

--- pkg/orchestrator/applications_handler.go
+++ pkg/orchestrator/applications_handler.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"github.com/gorilla/mux"
 	"github.com/hexa-org/policy-orchestrator/pkg/orchestrator/provider"
+	"log"
 	"net/http"
 	"strings"
 )
@@ -44,6 +45,7 @@ type ApplicationsHandler struct {
 func (handler ApplicationsHandler) List(w http.ResponseWriter, _ *http.Request) {
 	records, err := handler.applicationsGateway.Find()
 	if err != nil {
+		log.Println("Error accessing database: " + err.Error())
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
