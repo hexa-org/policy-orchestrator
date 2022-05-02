ccbf1f5b4fa4a90734f6f09f8bc353f7153d3c2c
barinek <barinek@initialcapacity.io>
2022-05-02 14:55:44 -0600

    moving demo to use decision support package
    Signed-off-by: barinek <barinek@initialcapacity.io>

--- pkg/decisionsupport/providers/opa_provider_test.go
+++ pkg/decisionsupport/providers/opa_provider_test.go
@@ -1,26 +1,79 @@
 package providers_test
 
 import (
+	"bytes"
+	"errors"
 	"github.com/hexa-org/policy-orchestrator/pkg/decisionsupport/providers"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"io/ioutil"
+	"net/http"
 	"testing"
 )
 
 func TestOpaDecisionProvider_BuildInput_BuildInput(t *testing.T) {
-	provider := providers.OpaDecisionProvider{}
-	defer func() {
-		if recover() == nil {
-			t.Fail()
-		}
-	}()
-	_, _ = provider.BuildInput(nil)
+	provider := providers.OpaDecisionProvider{
+		Principals: []interface{}{"allusers", "allauthenticatedusers", "sales@hexaindustries.io"},
+	}
+
+	req, _ := http.NewRequest("GET", "http://aDomain.com/noop", nil)
+	req.RequestURI = "/noop"
+	query, _ := provider.BuildInput(req)
+	casted := query.(providers.OpaQuery).Input
+	assert.Equal(t, "GET", casted["method"])
+	assert.Equal(t, "/noop", casted["path"])
+	assert.Equal(t, []interface{}{"allusers", "allauthenticatedusers", "sales@hexaindustries.io"}, casted["principals"])
+}
+
+type MockClient struct {
+	mock.Mock
+	response []byte
+	err      error
+}
+
+func (m *MockClient) Do(_ *http.Request) (*http.Response, error) {
+	r := ioutil.NopCloser(bytes.NewReader(m.response))
+	return &http.Response{StatusCode: 200, Body: r}, m.err
 }
 
 func TestOpaDecisionProvider_Allow(t *testing.T) {
-	provider := providers.OpaDecisionProvider{}
-	defer func() {
-		if recover() == nil {
-			t.Fail()
-		}
-	}()
-	_, _ = provider.Allow(nil)
+	mockClient := new(MockClient)
+	mockClient.response = []byte("{\"result\":true}")
+	provider := providers.OpaDecisionProvider{Client: mockClient, Url: "aUrl"}
+
+	req, _ := http.NewRequest("GET", "http://aDomain.com/noop", nil)
+	req.RequestURI = "/noop"
+	query, _ := provider.BuildInput(req)
+
+	allow, _ := provider.Allow(query)
+	assert.Equal(t, true, allow)
+}
+
+func TestOpaDecisionProvider_AllowWithRequestErr(t *testing.T) {
+	mockClient := new(MockClient)
+	mockClient.response = []byte("{\"result\":true}")
+	mockClient.err = errors.New("oops")
+	provider := providers.OpaDecisionProvider{Client: mockClient, Url: "aUrl"}
+
+	req, _ := http.NewRequest("GET", "http://aDomain.com/noop", nil)
+	req.RequestURI = "/noop"
+	query, _ := provider.BuildInput(req)
+
+	allow, err := provider.Allow(query)
+	assert.Equal(t, "oops", err.Error())
+	assert.Equal(t, false, allow)
+}
+
+func TestOpaDecisionProvider_AllowWithResponseErr(t *testing.T) {
+	mockClient := new(MockClient)
+	mockClient.response = []byte("__bad__ {\"result\":true}")
+	provider := providers.OpaDecisionProvider{Client: mockClient, Url: "aUrl"}
+
+	req, _ := http.NewRequest("GET", "http://aDomain.com/noop", nil)
+	req.RequestURI = "/noop"
+	query, _ := provider.BuildInput(req)
+
+	allow, err := provider.Allow(query)
+	assert.Equal(t, "invalid character '_' looking for beginning of value", err.Error())
+	assert.Equal(t, false, allow)
 }
