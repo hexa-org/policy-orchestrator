f5cba9964957b35dfd7e6a8475a96bdbb447625b
barinek <barinek@initialcapacity.io>
2022-05-05 09:54:16 -0600

    moving policy info to an isolated package
    Signed-off-by: barinek <barinek@initialcapacity.io>

--- pkg/orchestrator/test/doubles.go
+++ pkg/orchestrator/test/doubles.go
@@ -1,6 +1,7 @@
 package orchestrator_test
 
 import (
+	"github.com/hexa-org/policy-orchestrator/pkg/identityquerylanguage"
 	"github.com/hexa-org/policy-orchestrator/pkg/orchestrator/provider"
 )
 
@@ -22,13 +23,13 @@ func (n *NoopProvider) DiscoverApplications(info provider.IntegrationInfo) (apps
 	return apps, n.Err
 }
 
-func (n *NoopProvider) GetPolicyInfo(_ provider.IntegrationInfo, _ provider.ApplicationInfo) ([]provider.PolicyInfo, error) {
-	return []provider.PolicyInfo{
-		{"aVersion", "anAction", provider.SubjectInfo{AuthenticatedUsers: []string{"aUser"}}, provider.ObjectInfo{Resources: []string{"/"}}},
-		{"aVersion", "anotherAction", provider.SubjectInfo{AuthenticatedUsers: []string{"anotherUser"}}, provider.ObjectInfo{Resources: []string{"/"}}},
+func (n *NoopProvider) GetPolicyInfo(_ provider.IntegrationInfo, _ provider.ApplicationInfo) ([]identityquerylanguage.PolicyInfo, error) {
+	return []identityquerylanguage.PolicyInfo{
+		{"aVersion", "anAction", identityquerylanguage.SubjectInfo{AuthenticatedUsers: []string{"aUser"}}, identityquerylanguage.ObjectInfo{Resources: []string{"/"}}},
+		{"aVersion", "anotherAction", identityquerylanguage.SubjectInfo{AuthenticatedUsers: []string{"anotherUser"}}, identityquerylanguage.ObjectInfo{Resources: []string{"/"}}},
 	}, n.Err
 }
 
-func (n *NoopProvider) SetPolicyInfo(_ provider.IntegrationInfo, _ provider.ApplicationInfo, _ []provider.PolicyInfo) error {
+func (n *NoopProvider) SetPolicyInfo(_ provider.IntegrationInfo, _ provider.ApplicationInfo, _ []identityquerylanguage.PolicyInfo) error {
 	return n.Err
 }
