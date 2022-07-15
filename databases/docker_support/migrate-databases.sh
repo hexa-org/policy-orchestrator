2cbcd965caf7b46b8fc3353ee36b658fff9f7c51
barinek <barinek@initialcapacity.io>
2022-07-15 12:07:01 -0600

    tls support for orchestrator postgres client
    Signed-off-by: barinek <barinek@initialcapacity.io>

--- databases/docker_support/migrate-databases.sh
+++ databases/docker_support/migrate-databases.sh
@@ -5,4 +5,4 @@ while ! pg_isready --user orchestrator --host postgresql &> /dev/null; do
   sleep 2
   echo "Waiting for the orchestrator database to become active."
 done
-migrate -verbose -path /home/databases/orchestrator -database postgres://orchestrator:orchestrator@postgresql:5432/orchestrator_development?sslmode=disable up
+migrate -verbose -path '/home/databases/orchestrator' -database 'postgres://orchestrator:orchestrator@postgresql:5432/orchestrator_development?sslmode=require&sslcert=/home/databases/docker_support/client-cert.pem&sslkey=/home/databases/docker_support/client-key.pem' up
