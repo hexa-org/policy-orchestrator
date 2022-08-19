42dd15447ad77a6b4bf3976c460e090c398b58a9
Warren Fernandes <warren.f.fernandes@gmail.com>
2022-08-19 09:04:49 -0600

    Template renderer operates on a filesystem instead of dir path
    Update the tests to use an embed.FS to render templates
    
    Signed-off-by: Warren Fernandes <warren.f.fernandes@gmail.com>

--- pkg/websupport/view_support_test.go
+++ pkg/websupport/view_support_test.go
@@ -2,33 +2,39 @@ package websupport_test
 
 import (
 	"bytes"
-	"github.com/gorilla/mux"
-	"github.com/hexa-org/policy-orchestrator/pkg/websupport"
-	"github.com/stretchr/testify/assert"
 	"io"
 	"net"
 	"net/http/httptest"
-	"path/filepath"
-	"runtime"
 	"testing"
+
+	"github.com/gorilla/mux"
+	"github.com/hexa-org/policy-orchestrator/pkg/websupport"
+	"github.com/hexa-org/policy-orchestrator/pkg/websupport/test"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestModelAndView(t *testing.T) {
-	_, file, _, _ := runtime.Caller(0)
-	resourcesDirectory := filepath.Join(file, "../../../pkg/websupport/test")
-	options := websupport.Options{ResourceDirectory: resourcesDirectory}
-
 	listener, _ := net.Listen("tcp", "localhost:0")
-	websupport.Create(listener.Addr().String(), func(x *mux.Router) {}, options)
+	websupport.Create(listener.Addr().String(), func(x *mux.Router) {}, websupport.Options{})
 	writer := &httptest.ResponseRecorder{Body: new(bytes.Buffer)}
 
-	_ = websupport.ModelAndView(writer, "test", websupport.Model{Map: map[string]interface{}{"resource": "resource"}})
+	_ = websupport.ModelAndView(
+		writer,
+		&websupport_test.Resources,
+		"test",
+		websupport.Model{Map: map[string]interface{}{"resource": "resource"}},
+	)
 	body, _ := io.ReadAll(writer.Body)
 	assert.Contains(t, string(body), "success!")
 	assert.Contains(t, string(body), "Resource")
 	assert.Contains(t, string(body), "contains")
 	assert.Contains(t, string(body), "nope")
 
-	err := websupport.ModelAndView(&httptest.ResponseRecorder{}, "bad", websupport.Model{})
+	err := websupport.ModelAndView(
+		&httptest.ResponseRecorder{},
+		&websupport_test.Resources,
+		"bad",
+		websupport.Model{},
+	)
 	assert.Contains(t, err.Error(), "can't evaluate field Ba")
 }
