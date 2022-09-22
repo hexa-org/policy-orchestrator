36bdcafeda2594a949f5125a0c81d6b911e157a8
Corey Innis <corey@geometer.io>
2022-09-22 14:55:33 -0700

    [Added] `pkg` CLI
    A few key points of interest:
    
    - Utilities that are relevant to both development and running of Hexa are moved to the `pkg` CLI, away from the `dev` CLI.
    - Attempts have been made to make package setup tasks runnable when bootstrapping (e.g., first run as `./bin/kg setup` after cloning the repo).
    
    Note that the "setup" task assumes that the consumer is comfortable with the script/CLI installing a fair bit of software such as:
    
      - brew
      - asdf
      - direnv
    
    These and more provide mechanisms for installing software required for running Hexa, and some for improving the workspace ergonomics. An example: once that setup is run, `pkg serve` may be run from within any directory in the project codebase.
    
    Signed-off-by: Corey Innis <coreyti@me.com>

--- bin/dev.d/setup.sh
+++ bin/dev.d/setup.sh
@@ -9,7 +9,12 @@ source ${REPO}/bin/support.sh
 #
 # Description:
 #
-#   Perform workspace/dev post-install setup tasks.
+#   Perform workspace development setup tasks.
+#
+#   NOTE:
+#
+#   The project workspace must be bootstrapped with
+#   \`pkg setup\` before running these \`dev\` CLI tasks.
 #
 # Options:
 #
@@ -17,6 +22,7 @@ source ${REPO}/bin/support.sh
 #                             One of:
 #                               - all (default)
 #                               - db
+#                               - opa
 #   -h, --help                Print help text.
 # ------------------------------------------------------------------------------
 
@@ -30,6 +36,9 @@ _main_() {
     db )
       ensure::db
       ;;
+    opa )
+      ensure::opa
+      ;;
     * )
       echo::fail "unknown setup target: '${target}'"
       ;;
@@ -38,13 +47,14 @@ _main_() {
 
 ensure::all() {
   ensure::db
+  ensure::opa
 }
 
 # db
 # ----------------------------------------------------------
 ensure::db() {
-  check::db_user || setup::db_user
-  check::db_data || setup::db_data
+  exec::stat "DB user" "check::db_user" "setup::db_user"
+  exec::stat "DB data" "check::db_data" "setup::db_data"
   exec::step "Running DB migrations" "migrate -verbose -path ${REPO}/databases/orchestrator -database 'postgres://orchestrator:orchestrator@localhost:5432/orchestrator_test?sslmode=disable' up"
 }
 
@@ -64,6 +74,22 @@ setup::db_data() {
   createdb orchestrator_test --owner orchestrator
   psql --quiet --command="alter user orchestrator with password 'orchestrator'"
   psql --quiet --command="grant all privileges on database orchestrator_test to orchestrator"
+}
+
+# opa
+# ----------------------------------------------------------
+ensure::opa() {
+  exec::stat "opa" "command -v opa" "install::opa"
+}
+
+install::opa() {
+  pushd /tmp > /dev/null
+    curl -L -o opa_darwin_amd64 https://openpolicyagent.org/downloads/v0.44.0/opa_darwin_amd64
+    curl -L -o opa_darwin_amd64.sha256 https://openpolicyagent.org/downloads/v0.44.0/opa_darwin_amd64.sha256
+    shasum -c opa_darwin_amd64.sha256
+    chmod +x opa_darwin_amd64
+    mv opa_darwin_amd64 "$(brew --prefix)/bin/opa2"
+  popd > /dev/null
 }
 
 _main_ "$@"
