137fc1d6f4565d1b5b4d7eab657088b0cac23ffd
Warren Fernandes <warren.f.fernandes@gmail.com>
2022-11-07 12:05:48 -0700

    Use GCP Http Client library instead of storage library
    This allows us to TDD using HTTPClient interface and parse API responses appropriately.
    This also removes the heavy storage library dependency.
    
    Signed-off-by: Warren Fernandes <warren.f.fernandes@gmail.com>

---
+++ pkg/orchestratorproviders/openpolicyagent/http_bundle_client.go
@@ -0,0 +1,62 @@
+package openpolicyagent
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"mime/multipart"
+	"net/http"
+	"net/url"
+	"os"
+	"path/filepath"
+
+	"github.com/hexa-org/policy-orchestrator/pkg/compressionsupport"
+)
+
+type HTTPClient interface {
+	Get(url string) (resp *http.Response, err error)
+	Post(url, contentType string, body io.Reader) (resp *http.Response, err error)
+}
+
+type HTTPBundleClient struct {
+	BundleServerURL string
+	HttpClient      HTTPClient
+}
+
+func (b *HTTPBundleClient) GetDataFromBundle(path string) ([]byte, error) {
+	get, getErr := b.HttpClient.Get(b.BundleServerURL)
+	if getErr != nil {
+		return nil, getErr
+	}
+
+	all, readErr := io.ReadAll(get.Body)
+	if readErr != nil {
+		return nil, readErr
+	}
+
+	gz, gzipErr := compressionsupport.UnGzip(bytes.NewReader(all))
+	if gzipErr != nil {
+		return nil, gzipErr
+	}
+
+	tarErr := compressionsupport.UnTarToPath(bytes.NewReader(gz), path)
+	if tarErr != nil {
+		return nil, tarErr
+	}
+	return os.ReadFile(filepath.Join(path, "/bundle/data.json"))
+}
+
+// todo - ignoring errors for the moment while spiking
+
+func (b *HTTPBundleClient) PostBundle(bundle []byte) (int, error) {
+	// todo - Log out the errors at minimum.
+	buf := new(bytes.Buffer)
+	writer := multipart.NewWriter(buf)
+	formFile, _ := writer.CreateFormFile("bundle", "bundle.tar.gz")
+	_, _ = formFile.Write(bundle)
+	_ = writer.Close()
+	parse, _ := url.Parse(b.BundleServerURL)
+	contentType := writer.FormDataContentType()
+	resp, err := b.HttpClient.Post(fmt.Sprintf("%s://%s/bundles", parse.Scheme, parse.Host), contentType, buf)
+	return resp.StatusCode, err
+}
---
+++ pkg/orchestratorproviders/openpolicyagent/test/mock_httpclient.go
@@ -0,0 +1,44 @@
+package openpolicyagent_test
+
+import (
+	"bytes"
+	"io"
+	"net/http"
+
+	"github.com/stretchr/testify/mock"
+)
+
+type MockHTTPClient struct {
+	mock.Mock
+	Err          error
+	ResponseBody map[string][]byte
+	RequestBody  []byte
+	Url          string
+	StatusCode   int
+}
+
+func NewMockHTTPClient() *MockHTTPClient {
+	return &MockHTTPClient{
+		Mock:         mock.Mock{},
+		Err:          nil,
+		ResponseBody: make(map[string][]byte),
+		RequestBody:  nil,
+		Url:          "",
+		StatusCode:   200,
+	}
+}
+
+func (m *MockHTTPClient) Get(url string) (resp *http.Response, err error) {
+	m.Url = url
+	var body []byte
+	body = m.ResponseBody[url]
+	return &http.Response{StatusCode: 200, Body: io.NopCloser(bytes.NewReader(body))}, m.Err
+}
+
+func (m *MockHTTPClient) Post(url, _ string, body io.Reader) (resp *http.Response, err error) {
+	m.Url = url
+	m.RequestBody, _ = io.ReadAll(body)
+	var responseBody []byte
+	responseBody = m.ResponseBody[url]
+	return &http.Response{StatusCode: m.StatusCode, Body: io.NopCloser(bytes.NewReader(responseBody))}, m.Err
+}
