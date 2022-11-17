31e41b775fde5b591f019f933ea198e78f68f078
Dylan Ham <dylan@initialcapacity.com>
2022-11-17 11:17:01 -0700

    Bugfix for orchestration across google and azure
    Signed-off-by: Dylan Ham <dylan@initialcapacity.com>

--- pkg/orchestrator/applications_service_test.go
+++ pkg/orchestrator/applications_service_test.go
@@ -18,9 +18,10 @@ type applicationsServiceData struct {
 	db        *sql.DB
 	providers map[string]orchestrator.Provider
 
-	fromApp        string
-	toApp          string
-	toAppDifferent string
+	fromNoopApp string
+	toNoopApp   string
+	azureApp    string
+	googleApp   string
 }
 
 func (data *applicationsServiceData) SetUp() {
@@ -33,7 +34,10 @@ insert into applications (id, integration_id, object_id, name, description, serv
 insert into applications (id, integration_id, object_id, name, description, service) values ('6409776a-367a-483a-a194-5ccf9c4ff211', '50e00619-9f15-4e85-a7e9-f26d87ea12e7', 'anotherObjectId', 'anotherName', 'anotherDescription', 'anotherService');
 
 insert into integrations (id, name, provider, key) values ('50e00619-9f15-4e85-a7e9-f26d87ea12e8', 'anotherName', 'azure', 'aKey');
-insert into applications (id, integration_id, object_id, name, description, service) values ('6409776a-367a-483a-a194-5ccf9c4ff212', '50e00619-9f15-4e85-a7e9-f26d87ea12e8', 'andAnotherObjectId', 'andAnotherName', 'andAnotherDescription', 'yetAnotherService');
+insert into applications (id, integration_id, object_id, name, description, service) values ('6409776a-367a-483a-a194-5ccf9c4ff212', '50e00619-9f15-4e85-a7e9-f26d87ea12e8', 'andAnotherObjectId', 'andAnotherName', 'andAnotherDescription', 'andAnotherService');
+
+insert into integrations (id, name, provider, key) values ('50e00619-9f15-4e85-a7e9-f26d87ea12e9', 'yetAnotherName', 'google_cloud', 'aKey');
+insert into applications (id, integration_id, object_id, name, description, service) values ('6409776a-367a-483a-a194-5ccf9c4ff213', '50e00619-9f15-4e85-a7e9-f26d87ea12e9', 'yetAnotherObjectId', 'yetAnotherName', 'yetAnotherDescription', 'yetAnotherService');
 `)
 
 	// TODO: pass `testing.T` into this `SetUp`.
@@ -42,12 +46,15 @@ insert into applications (id, integration_id, object_id, name, description, serv
 		log.Fatalf("failed to setup DB: %v", err)
 	}
 
-	data.fromApp = "6409776a-367a-483a-a194-5ccf9c4ff210"
-	data.toApp = "6409776a-367a-483a-a194-5ccf9c4ff211"
-	data.toAppDifferent = "6409776a-367a-483a-a194-5ccf9c4ff212"
+	data.fromNoopApp = "6409776a-367a-483a-a194-5ccf9c4ff210"
+	data.toNoopApp = "6409776a-367a-483a-a194-5ccf9c4ff211"
+	data.azureApp = "6409776a-367a-483a-a194-5ccf9c4ff212"
+	data.googleApp = "6409776a-367a-483a-a194-5ccf9c4ff213"
 
 	data.providers = make(map[string]orchestrator.Provider)
 	data.providers["noop"] = &orchestrator_test.NoopProvider{}
+	data.providers["azure"] = &orchestrator_test.NoopProvider{OverrideName: "azure"}
+	data.providers["google_cloud"] = &orchestrator_test.NoopProvider{OverrideName: "google_cloud"}
 }
 
 func (data *applicationsServiceData) TearDown() {
@@ -60,18 +67,21 @@ func TestApplicationsService_Apply(t *testing.T) {
 		integrationsGateway := orchestrator.IntegrationsDataGateway{DB: data.db}
 		applicationsService := orchestrator.ApplicationsService{ApplicationsGateway: applicationsGateway, IntegrationsGateway: integrationsGateway, Providers: data.providers}
 
-		err := applicationsService.Apply(orchestrator.Orchestration{From: data.fromApp, To: data.toApp})
-		assert.NoError(t, err)
+		sameProviders := applicationsService.Apply(orchestrator.Orchestration{From: data.fromNoopApp, To: data.toNoopApp})
+		assert.NoError(t, sameProviders)
 
-		badFromApp := applicationsService.Apply(orchestrator.Orchestration{From: "", To: data.toApp})
+		differentProviders := applicationsService.Apply(orchestrator.Orchestration{From: data.azureApp, To: data.googleApp})
+		assert.NoError(t, differentProviders)
+
+		badFromApp := applicationsService.Apply(orchestrator.Orchestration{From: "", To: data.toNoopApp})
 		assert.Error(t, badFromApp)
 
-		badToApp := applicationsService.Apply(orchestrator.Orchestration{From: data.fromApp, To: ""})
+		badToApp := applicationsService.Apply(orchestrator.Orchestration{From: data.fromNoopApp, To: ""})
 		assert.Error(t, badToApp)
 
 		data.providers["noop"] = &orchestrator_test.NoopProvider{Err: errors.New("oops")}
 
-		providerError := applicationsService.Apply(orchestrator.Orchestration{From: data.fromApp, To: data.toApp})
+		providerError := applicationsService.Apply(orchestrator.Orchestration{From: data.fromNoopApp, To: data.toNoopApp})
 		assert.Error(t, providerError)
 	})
 }
