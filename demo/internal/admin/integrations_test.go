c47da4be4f4822c7a55d2c8b2e7b699ffcee79a5
Phil Hunt <phil.hunt@independentid.com>
2024-05-19 18:25:06 -0700

    fixup! Issue #410, Remove redundant avp template.
    Signed-off-by: Phil Hunt <phil.hunt@independentid.com>

--- demo/internal/admin/integrations_test.go
+++ demo/internal/admin/integrations_test.go
@@ -2,7 +2,6 @@ package admin_test
 
 import (
 	"bytes"
-	"encoding/base64"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -146,7 +145,7 @@ func (suite *IntegrationsSuite) TestCreateIntegration_withOPA() {
 			resp, _ := http.Post(fmt.Sprintf("http://%s/integrations", suite.server.Addr), contentType, buf)
 			assert.Equal(suite.T(), http.StatusOK, resp.StatusCode)
 			assert.Equal(suite.T(), "open_policy_agent", suite.client.Provider)
-			assert.Equal(suite.T(), fmt.Sprintf("%s:open-policy-agent", base64.StdEncoding.EncodeToString([]byte("http://opa-bundle-url"))), suite.client.Name)
+			assert.Equal(suite.T(), "opa-bundle-url/:open-policy-agent", suite.client.Name)
 		})
 	}
 }
