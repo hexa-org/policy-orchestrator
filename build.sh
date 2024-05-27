1fbe3f8eb3f04644a78b7c8ce9f52bca3dab9ef8
Phil Hunt <phil.hunt@independentid.com>
2024-05-27 12:15:49 -0700

    Remove redundant build.sh
    Signed-off-by: Phil Hunt <phil.hunt@independentid.com>

--- build.sh
+++
@@ -1,8 +0,0 @@
-
-echo "Building and testing Policy-Orchestrator."
-echo "  if running manually, see demo/build.sh"
-# This is called by CodeQL Github Action
-
-cd ./demo
-source ./.env_development
-source ./build.sh -b
