b99dfcf4bb5d5a6787b0af2d360eda079bdde719
Phil Hunt <phil.hunt@independentid.com>
2024-06-08 11:53:41 -0700

    Fixed multi-threading test issue by putting mutex test_support.go
    Signed-off-by: Phil Hunt <phil.hunt@independentid.com>

--- demo/pkg/testsupport/test_support.go
+++ demo/pkg/testsupport/test_support.go
@@ -1,15 +1,23 @@
 package testsupport
 
-import "time"
+import (
+	"sync"
+	"time"
+)
 
 type TestData interface {
 	SetUp()
 	TearDown()
 }
 
+var mu sync.Mutex
+
 func WithSetUp[T TestData](data T, test func(data T)) {
+	mu.Lock()
+	defer mu.Unlock()
 	data.SetUp()
 	time.Sleep(50)
 	test(data)
 	data.TearDown()
+
 }
